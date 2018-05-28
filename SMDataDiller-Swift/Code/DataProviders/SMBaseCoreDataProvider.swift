f03fe8e2639dbf5cb17868f81e4fcb544bfe369c
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2018-05-28 20:22:51 +0300

    Add project

---
+++ SMDataDiller-Swift/Code/DataProviders/SMBaseCoreDataProvider.swift
@@ -0,0 +1,9 @@
+//
+//  SMBaseCoreDataProvider.swift
+//  SMDataDiller-Swift
+//
+//  Created by Max on 28.05.2018.
+//  Copyright © 2018 molfar.io. All rights reserved.
+//
+
+import Foundation
---
+++ SMDataDiller-Swift/Code/DataSources/SMBaseDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  SMBaseDataSource.swift
+//  SMDataDiller-Swift
+//
+//  Created by Max on 28.05.2018.
+//  Copyright © 2018 molfar.io. All rights reserved.
+//
+
+import Foundation
